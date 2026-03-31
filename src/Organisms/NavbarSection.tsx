08f8b437c4b5f9967f7eabf89d9b18ed09f14ca6
mohamednkklk534 <elzerossss@gmail.com>
2026-03-31 02:54:01 +0200

    feat(navbar):Add navbar section

---
+++ src/Organisms/NavbarSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavbarSection = () => {
+  return (
+    <div className='bg-blue-900 text-5xl text-white flex justify-center items-center h-30'>Navbar</div>
+  )
+}
+
+export default NavbarSection
